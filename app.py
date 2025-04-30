e41f2abbc6b5b0f5f04b0742346184446b2139bd
thetanish1 <tanishdewase25@gmail.com>
2025-04-30 14:45:33 +0530

    added

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import os
 def db_connection():
     try:
         conn = mysql.connector.connect(
-    host=os.getenv("MYSQL_HOST", "localhost"),
     user=os.getenv("MYSQL_USER", "root"),
     password=os.getenv("MYSQL_PASSWORD", "root"),
     database=os.getenv("MYSQL_DATABASE", "department_management")
